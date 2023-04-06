9d17bd18f19c0c7d63f7176cb5521c8a312eeba7
Abylkaiyr <abylkaiyr.omar@alumni.nu.edu.kz>
2023-04-06 18:48:34 +0600

    if else statements
    added

--- js/app.js
+++ js/app.js
@@ -17,17 +17,53 @@
 
 // Zadacha 1
 
-const perHour = 80;
-const dailyWorkHour = 5;
-const weeklyWorkDays = 5;
-const totalHoursWork = 40;
-const remainingDays = 11;
+// const perHour = 80;
+// const dailyWorkHour = 5;
+// const weeklyWorkDays = 5;
+// const totalHoursWork = 40;
+// const remainingDays = 11;
 
-// Uspeete li vy?
-const daysToWork = totalHoursWork/dailyWorkHour
-const isPossible = (remainingDays-2)>daysToWork
-console.log(isPossible)
+// // Uspeete li vy?
+// const daysToWork = totalHoursWork/dailyWorkHour
+// const isPossible = (remainingDays-2)>daysToWork
+// console.log(isPossible)
 
-// Skolko poluchew?
-const cash = daysToWork*dailyWorkHour*perHour
-console.log(cash)
+// // Skolko poluchew?
+// const cash = daysToWork*dailyWorkHour*perHour
+// console.log(cash)
+
+// шаблонная Строка
+// const projectName = 'сайт свайп'
+// const price = 2000;
+// const author = 'Василий Пупкин'
+// console.log(`${author} заказал ${projectName} на сумму $${price}`)
+
+// If and Else statement
+
+// const money = 10
+// if (money>50) {
+//     console.log('Can buy product') 
+// } else if (money>40) {
+//     console.log('may buy product')
+// } else {
+//     console.log('Biwarawka')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
