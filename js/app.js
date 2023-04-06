110838278bf8ba3d651548ab86d04b915022b9ba
Abylkaiyr <abylkaiyr.omar@alumni.nu.edu.kz>
2023-04-06 19:23:12 +0600

    swithc case

--- js/app.js
+++ js/app.js
@@ -13,8 +13,6 @@
 // let d = null;
 // console.log(d == Object);
 
-
-
 // Zadacha 1
 
 // const perHour = 80;
@@ -42,28 +40,42 @@
 
 // const money = 10
 // if (money>50) {
-//     console.log('Can buy product') 
+//     console.log('Can buy product')
 // } else if (money>40) {
 //     console.log('may buy product')
 // } else {
 //     console.log('Biwarawka')
 // }
 
+// Zadacha 2
 
+// const initialCash = 10000;
+// const debt = 7 / 100;
+// const finalCash = 13500;
+// const month = 24;
 
+// const d = initialCash * (1 + 0.07/12) ** month;
 
+// if (d >= finalCash) {
+//   console.log(
+//     `Вася сможет купить дом уложившись в срок и при этом у него останется $${
+//       d - finalCash
+//     } денег`
+//   );
+// } else {
+//   console.log(`Васе не хватает столько денег ${finalCash - d}`);
+// }
 
+// Switch case
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+const role = "qdmin";
+switch (role) {
+  case "Manager":
+    console.log("I am manager");
+    break;
+  case "Admin":
+    console.log("admin");
+    break;
+  default:
+    console.log("Bezrabotny");
+}
