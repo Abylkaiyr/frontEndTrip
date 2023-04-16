276bcae706e63063d5a5627c118f82064db2ab5f
Abylkaiyr <abylkaiyr.omar@alumni.nu.edu.kz>
2023-04-16 11:32:05 +0600

    Changes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-'use strict' // Ограничить старые конвенций JS
+'use strict'
+const audi = {
+    make: 'audi',
+    model: 'A3',
+    year: 2009,
+    damages: [],
+    addDamage(part, rate){
+        this.damages.push({
+            part,
+            rate
+        })
+    }
+}
+
+audi.addDamage('Капот', 1)
